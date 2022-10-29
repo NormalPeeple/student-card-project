1a093cf1f3df2ad80257161fa6069444c263e115
NormalPeeple <73377301+NormalPeeple@users.noreply.github.com>
2022-10-30 00:35:04 +0700

    Revert "Revert "🔊 Add more prints""

--- Background_Pixel_calculationn.py
+++ Background_Pixel_calculationn.py
@@ -7,16 +7,20 @@ from matplotlib.patches import Rectangle
 
 def onclick(event):
     print('heehee')
+    print('ehe')
     if event.button is MouseButton.LEFT:
         print('heehee')
         print('disconnecting callback')
+        print('ehe')
         plt.disconnect(binding_id)
 
 
 def on_move(event):
     print('heehee')
+    print('ehe')
     if event.inaxes:
         print('heehee')
+        print('ehe')
         print(f'data coords {event.xdata} {event.ydata},',
               f'pixel coords {event.x} {event.y}')
 
@@ -24,8 +28,10 @@ def on_move(event):
 def onMouse(event, x, y, flags, param):
     global posList
     print('heehee')
+    print('ehe')
     if event == cv.EVENT_LBUTTONDOWN:
         print('heehee')
+        print('ehe')
         print(f'x = {x} , y = {y}')
         posList.append((x, y))
 
@@ -33,26 +39,31 @@ def onMouse(event, x, y, flags, param):
 top_left = []
 print('heehee')
 bot_right = []
+print('ehe')
 
 
 def draw_square(event, x, y, flags, *userdata):
     global top_left, bot_right
     print('heehee')
+    print('ehe')
     if event == cv.EVENT_LBUTTONDOWN:
         top_left = [(x, y)]
         print('heehee')
         print('Top left', top_left)
+        print('ehe')
     elif event == cv.EVENT_LBUTTONUP:
         bot_right = [(x, y)]
         print('Bot left', bot_right)
         print('heehee')
         cv.rectangle(image, top_left[0], bot_right[0], (0, 255, 0), 2, 8)
         cv.imshow("Window", image)
+        print('ehe')
 
 
 def on_select(click, release):
     extent = rect_selector.extents
     print('heehee')
+    print('ehe')
     print('Extend = ', extent)
 
 
@@ -60,6 +71,7 @@ background = cv.imread('b_fis.jpg', cv.IMREAD_UNCHANGED)
 
 fig, ax = plt.subplots()
 print('heehee')
+print('ehe')
 ax.imshow(background[:, :, ::-1])
 
 rect_selector = RectangleSelector(
@@ -68,3 +80,4 @@ plt.show()
 
 print("Hello World")
 print('heehee')
+print('ehe')
